bb7a3a1d85e06fac7e97c47ad199bbddd6cb7144
Weuzskr <119879369+weuzskr@users.noreply.github.com>
2023-12-05 20:19:11 +0000

    Ajout du document !!

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 import cx_Oracle
+from config import ORACLE_CONFIG
 
 app = Flask(__name__)
 
-# Configuration de la base de données Oracle
-app.config['ORACLE_USER'] = 'OUSMANE'
-app.config['ORACLE_PASSWORD'] = '1234'
-app.config['ORACLE_DSN'] = 'votre_dsn'
+app.config['ORACLE_DSN'] = (
+    f"(DESCRIPTION="
+    f"    (ADDRESS=(PROTOCOL=TCP)(HOST={ORACLE_CONFIG['HOST']})(PORT={ORACLE_CONFIG['PORT']}))"
+    f"    (CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME={ORACLE_CONFIG['SERVICE_NAME']}))"
+    f")"
+)
 
 @app.route('/')
 def home():
@@ -25,20 +28,31 @@ def securite():
 
 @app.route('/supervision')
 def supervision():
-    # Connexion à la base de données Oracle
-    connection = cx_Oracle.connect(app.config['ORACLE_USER'], app.config['ORACLE_PASSWORD'], app.config['ORACLE_DSN'])
-    cursor = connection.cursor()
+    try:
+        connection = cx_Oracle.connect(
+            ORACLE_CONFIG['USER'],
+            ORACLE_CONFIG['PASSWORD'],
+            app.config['ORACLE_DSN']
+        )
+        cursor = connection.cursor()
 
-    # Ajoutez ici le code pour récupérer les informations de supervision depuis la base de données Oracle
-    # Par exemple : 
-    cursor.execute("SELECT * FROM table_supervision")
-    supervision_data = cursor.fetchall()  # Ceci est un exemple, adaptez-le en fonction de votre structure de données
+        # Ajoutez ici le code pour récupérer les informations de supervision depuis la base de données Oracle
+        # Par exemple :
+        cursor.execute("SELECT * FROM personne")
+        supervision_data = cursor.fetchall()  # Ceci est un exemple, adaptez-le en fonction de votre structure de données
 
-    # Fermez la connexion
-    cursor.close()
-    connection.close()
+        # Fermez la connexion
+        cursor.close()
+        connection.close()
 
-    return render_template('supervision.html', supervision_data=supervision_data)
+        message = "Connexion à la base de données réussie."
+
+    except cx_Oracle.Error as e:
+        # Gestion de l'erreur en cas d'échec de la connexion
+        print("Erreur de connexion à la base de données:", e)
+        message = "Échec de la connexion à la base de données."
+
+    return render_template('supervision.html', message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+ORACLE_CONFIG = {
+    'USER': 'ousmane',
+    'PASSWORD': '1234',
+    'HOST': 'DESKTOP-OUSMANE',
+    'PORT': '1521',
+    'SERVICE_NAME': 'orclteste',
+}
